bc05a9b38f195cd7f0e08792f7d141fbb6741b7d
David Siaw <davidsiaw@gmail.com>
2019-02-19 23:53:05 +0900

    activestorage volume and activejob queue adapter

--- config/application.rb
+++ config/application.rb
@@ -34,5 +34,6 @@ module RailsZen
 
     config.paths.add File.join('app', 'apis'), glob: File.join('**', '*.rb')
     config.autoload_paths += Dir[Rails.root.join('app', 'apis', '*')]
+    config.active_job.queue_adapter = :sidekiq
   end
 end
