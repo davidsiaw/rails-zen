3c964652860ecbea62dcb58c880cde3b3dd39e5d
David Siaw <davidsiaw@gmail.com>
2019-02-24 03:13:57 +0900

    spec generators

--- config/initializers/generators.rb
+++ config/initializers/generators.rb
@@ -4,4 +4,6 @@ Rails.application.config.generators do |g|
   g.orm :active_record, primary_key_type: :uuid
   g.stylesheets false
   g.javascripts false
+  g.test_framework :rspec
+  g.fixture_replacement :factory_bot, dir: 'spec/factories'
 end
