91c35ad27db13d7d73ac0e3f851c0c2a8d0c253c
David Siaw <davidsiaw@gmail.com>
2019-03-11 02:27:55 +0900

    fix model generation, add shoulda, concern helper and constants

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -1,8 +1,8 @@
 # frozen_string_literal: true
 
 # This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV['RAILS_ENV'] = 'test'
 require 'spec_helper'
-ENV['RAILS_ENV'] ||= 'test'
 require File.expand_path('../config/environment', __dir__)
 # Prevent database truncation if the environment is production
 abort('The Rails environment is running in production mode!') if Rails.env.production?
@@ -23,7 +23,7 @@ require 'database_cleaner'
 # directory. Alternatively, in the individual `*_spec.rb` files, manually
 # require only the support files necessary.
 #
-# Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
+Dir[Rails.root.join('spec', 'support', '**', '*.rb')].each { |f| require f }
 
 # Checks for pending migrations and applies them before tests are run.
 # If you are not using ActiveRecord, you can remove these lines.
@@ -34,6 +34,9 @@ rescue ActiveRecord::PendingMigrationError => e
   exit 1
 end
 RSpec.configure do |config|
+  # Remember to include global test helpers here
+  include ConcernHelper
+
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
---
+++ spec/support/helpers/concern_helper.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+module ConcernHelper
+  def spec_dir
+    File.dirname metadata[:file_path]
+  end
+
+  def it_has_concern(concern_name)
+    require Rails.root.join "#{spec_dir}/concerns/#{concern_name.underscore}"
+    it_behaves_like concern_name
+  end
+end
