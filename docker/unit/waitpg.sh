dbec8a0e0b9de5b4d79437f8f9e24941f3bb3786
David Siaw <davidsiaw@gmail.com>
2019-07-07 00:58:33 +0900

    fix timeout command

--- docker/unit/waitpg.sh
+++ docker/unit/waitpg.sh
@@ -1,1 +1,1 @@
-timeout sh -c 'until nc -z $0 $1; do sleep 1; echo "Waiting for Postgres"; done' localhost 5432; 
+timeout 10 sh -c 'until nc -z $0 $1; do sleep 1; echo "Waiting for Postgres"; done' localhost 5432; 
