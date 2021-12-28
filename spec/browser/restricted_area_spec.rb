c42bcd413ebe66141786306e67d7133427bb0b3b
David Siaw <davidsiaw@gmail.com>
2021-12-28 15:42:05 +0900

    appease rubocop

--- spec/browser/restricted_area_spec.rb
+++ spec/browser/restricted_area_spec.rb
@@ -4,7 +4,7 @@ require 'rails_helper'
 require 'browser_helper'
 
 RSpec.describe 'Restricted Area', :js, type: :browser do
-require 'tataru'
+  require 'tataru'
   let(:admin) { create :admin }
 
   it 'sidekiq cannot be accessed without logging in' do
